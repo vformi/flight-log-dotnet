1cfc0783e1782c179fd9e616447e2afe0d35a90f
Pavel Sušický <me@frontendista.cz>
2024-11-18 15:05:09 +0100

    feat(5.1): add csv report

--- FlightLogNet/Operation/GetExportToCsvOperation.cs
+++ FlightLogNet/Operation/GetExportToCsvOperation.cs
@@ -1,16 +1,66 @@
-﻿namespace FlightLogNet.Operation
+﻿using System;
+using System.Text;
+using FlightLogNet.Models;
+
+namespace FlightLogNet.Operation
 {
     using Repositories.Interfaces;
 
     public class GetExportToCsvOperation(IFlightRepository flightRepository)
     {
+        private const char Separator = ';';
+        private readonly string header = string.Join(Separator,
+            ReportLocalization.FlightIdColumn,
+            ReportLocalization.DateColumn,
+            ReportLocalization.TypeColumn,
+            ReportLocalization.ImmatriculationColumn,
+            ReportLocalization.PilotColumn,
+            ReportLocalization.CopilotColumn,
+            ReportLocalization.TaskColumn,
+            ReportLocalization.TakeoffTimeColumn,
+            ReportLocalization.LandingTimeColumn,
+            ReportLocalization.FlightLengthColumn,
+            Environment.NewLine
+        );
+
         public byte[] Execute()
         {
-            // TODO 5.1: Naimplementujte export do CSV
-            // TIP: CSV soubor je pouze string, který se dá vytvořit pomocí třídy StringBuilder
-            // TIP: Do bytové reprezentace je možné jej převést například pomocí metody: Encoding.UTF8.GetBytes(..)
+            StringBuilder builder = new(header);
 
-            return new byte[0];
+            var reports = flightRepository.GetReport();
+            
+            foreach (var report in reports)
+            {
+                AddRow(builder, report.Towplane);
+                
+                if (report.Glider != null)
+                {
+                    AddRow(builder, report.Glider);
+                }
+            }
+
+            return Encoding.UTF8.GetBytes(builder.ToString());
+        }
+
+        private static void AddRow(StringBuilder builder, FlightModel flight)
+        {
+            builder.Append($"{flight.Id}{Separator}");
+            builder.Append($"{flight.TakeoffTime.ToString("dd. MM. yyyy")}{Separator}");
+            builder.Append($"{flight.Airplane.Type}{Separator}");
+            builder.Append($"{flight.Airplane.Immatriculation}{Separator}");
+            builder.Append($"{flight.Pilot.LastName}, {flight.Pilot.FirstName}{Separator}");
+            builder.Append($"{flight.Copilot?.LastName ?? string.Empty}{Separator}");
+            builder.Append($"{flight.Task}{Separator}");
+            builder.Append($"{flight.TakeoffTime.ToString("HH:mm:ss")}");
+
+            if (flight.LandingTime != null)
+            {
+                builder.Append(Separator);
+                builder.Append($"{flight.LandingTime.Value.ToString("HH:mm:ss")}{Separator}");
+                builder.Append($"{flight.LandingTime - flight.TakeoffTime}");
+            }
+
+            builder.Append(Environment.NewLine);
         }
     }
 }
